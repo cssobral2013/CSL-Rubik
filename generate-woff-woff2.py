27ef905653c42b304db9ef5494fe2ebcb001ef62
Cristiano Sobral <cssobral2013@gmail.com>
2025-02-02 01:17:01 -0300

    Upgrade to v2.0

---
+++ generate-woff-woff2.py
@@ -0,0 +1,304 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+
+srcPath  = './ttf/CSLRubik-ExtraLight.ttf'
+woffPath = './web/CSLRubik-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CSLRubik-ExtraLightItalic.ttf'
+woffPath = './web/CSLRubik-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/CSLRubik-Light.ttf'
+woffPath = './web/CSLRubik-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-LightItalic.ttf'
+woffPath = './web/CSLRubik-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-LightItalic.ttf'
+woffPath = './web/CSLRubik-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-Regular.ttf'
+woffPath = './web/CSLRubik-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-Italic.ttf'
+woffPath = './web/CSLRubik-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-Medium.ttf'
+woffPath = './web/CSLRubik-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-MediumItalic.ttf'
+woffPath = './web/CSLRubik-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-SemiBold.ttf'
+woffPath = './web/CSLRubik-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-SemiBoldItalic.ttf'
+woffPath = './web/CSLRubik-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-SemiBold.ttf'
+woffPath = './web/CSLRubik-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-SemiBoldItalic.ttf'
+woffPath = './web/CSLRubik-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-Bold.ttf'
+woffPath = './web/CSLRubik-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-BoldItalic.ttf'
+woffPath = './web/CSLRubik-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-ExtraBold.ttf'
+woffPath = './web/CSLRubik-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-ExtraBoldItalic.ttf'
+woffPath = './web/CSLRubik-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+ 
+srcPath  = './ttf/CSLRubik-Black.ttf'
+woffPath = './web/CSLRubik-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLRubik-BlackItalic.ttf'
+woffPath = './web/CSLRubik-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+
+srcPath  = './ttf/CSLRubik-ExtraLight.ttf'
+woff2Path = './web/CSLRubik-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/CSLRubik-ExtraLightItalic.ttf'
+woff2Path = './web/CSLRubik-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/CSLRubik-Light.ttf'
+woff2Path = './web/CSLRubik-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-LightItalic.ttf'
+woff2Path = './web/CSLRubik-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-LightItalic.ttf'
+woff2Path = './web/CSLRubik-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-Regular.ttf'
+woff2Path = './web/CSLRubik-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-Italic.ttf'
+woff2Path = './web/CSLRubik-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-Medium.ttf'
+woff2Path = './web/CSLRubik-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-MediumItalic.ttf'
+woff2Path = './web/CSLRubik-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-SemiBold.ttf'
+woff2Path = './web/CSLRubik-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-SemiBoldItalic.ttf'
+woff2Path = './web/CSLRubik-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-SemiBold.ttf'
+woff2Path = './web/CSLRubik-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-SemiBoldItalic.ttf'
+woff2Path = './web/CSLRubik-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-Bold.ttf'
+woff2Path = './web/CSLRubik-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-BoldItalic.ttf'
+woff2Path = './web/CSLRubik-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-ExtraBold.ttf'
+woff2Path = './web/CSLRubik-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-ExtraBoldItalic.ttf'
+woff2Path = './web/CSLRubik-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+srcPath  = './ttf/CSLRubik-Black.ttf'
+woff2Path = './web/CSLRubik-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLRubik-BlackItalic.ttf'
+woff2Path = './web/CSLRubik-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path) 
